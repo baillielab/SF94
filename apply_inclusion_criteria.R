4af37d4c6c4757b7360decef911b84245ce8a918
RenaissancePleb <71074435+RenaissancePleb@users.noreply.github.com>
2021-02-03 08:39:06 +0000

    _

--- apply_inclusion_criteria.R
+++ apply_inclusion_criteria.R
@@ -1,5 +1,29 @@
+library(dplyr)
+
+library(data.table)
+
+# Steven's totally awesome alternate code suggestion
+# I'd also recommend deleting this file and putting the following code at the start of
+# evaluate_outcomes
+
+df_1<-fread("/home/u034/mcswets/df_1_20210202.csv", data.table = FALSE)
+
+subjects_to_include <- filter(df, ( !is.na(sao2) & days_since_admission %in% c(0,1,2,3)  & age_estimateyears >19 & age_estimateyears <76 ) )$subjid
+
+subjects_to_include <- unique(subjects_to_include)
+
+
+
 #data used:
 df_1<-read.csv("/home/u034/mcswets/df_1_20210202.csv")
+
+
+df_1<-read.csv("Y:/mcswets/df_1_20210202.csv")
+
+df_2 <- fread("Y:/mcswets/df_1_20210202.csv", data.table = FALSE)
+
+
+
 
 #select subjid of patients with a sats measurement on day 0,1,2 or 3
 subjects_to_include<-subset(df_1,((!is.na(sao2) & days_since_admission == 0)|
