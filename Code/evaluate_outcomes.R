0fb9dce65b6667e552a1db5c566fef3c2bcdba29
Maaike Swets <64903093+maaikeswets@users.noreply.github.com>
2021-04-07 20:14:40 +0200

    ---

--- Code/evaluate_outcomes.R
+++ Code/evaluate_outcomes.R
@@ -785,10 +785,9 @@ boot_result_sus_0.70 <- boot(data = subset1, statistic = effect_size_boot_sus, R
 sus1_booted_0.70<-boot.ci(boot_result_sus_0.70, conf = 0.95, type="basic")
 
 #numbers for Natalie
-write.csv(effectsize_sus_improvement,"/home/skerr/Git/SF94/Outputs/effectsize_sus_improvement.csv")
+write.csv(eeffectsize_susump1,"/home/skerr/Git/SF94/Outputs/effectsize_susump1.csv")
 p1_1L_number <- sum(subset1$sustained_1L_improvement == 1, na.rm = T)/ sum(!is.na(subset1$sustained_1L_improvement))
 write.csv(p1_1L_number,"/home/skerr/Git/SF94/Outputs/p1_1L_number.csv")
-
 
 susimp_pwr_func<-function(subset_df, effectsize_1L, effectsize_2L){
   p1_1L <- sum(subset_df$sustained_1L_improvement == 1, na.rm = T)/ sum(!is.na(subset_df$sustained_1L_improvement))
